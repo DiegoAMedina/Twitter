e9c8bde71e417a20c789272d92a9353836391d1b
DiegoAMedina <dimedina@csumb.edu>
2018-03-10 23:32:18 -0800

    added the profile view controller, but need to add the outlets and information

---
+++ twitter_alamofire_demo/ProfileViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileViewController.swift
+//  twitter_alamofire_demo
+//
+//  Created by Diego Medina on 3/10/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+// TODO: PASS ALL THE INFORMATION TO THE PROFILE VIEW CONTROLLER
+
+
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
